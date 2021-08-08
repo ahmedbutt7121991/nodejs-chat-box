0428543e1fab58eb3d7d8d9b7d5a36dbc76ee3e4
ahmedbutt <ahmed.butt91@gmail.com>
2021-08-08 22:28:13 +0000

    ch5_04

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 const app = express()
 
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
 const messages = [
     {"name": "ahmed", "message": "Hello"},
@@ -10,6 +13,11 @@ const messages = [
 
 app.get("/messages", (req, res) => {
     res.send(messages)
+})
+
+app.post("/messages", (req, res) => {
+    messages.push(req.body)
+    res.sendStatus(200)
 })
 
 const server = app.listen(3000, () => {
