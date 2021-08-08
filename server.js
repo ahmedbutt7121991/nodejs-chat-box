b69c5fd95e7a5ddbb053e6f93deb95707af993fc
ahmedbutt <ahmed.butt91@gmail.com>
2021-08-08 22:05:53 +0000

    ch5_03

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const app = express()
 
 app.use(express.static(__dirname))
 
+const messages = [
+    {"name": "ahmed", "message": "Hello"},
+    {"name": "ahsan", "message": "Hello"}
+]
+
+app.get("/messages", (req, res) => {
+    res.send(messages)
+})
+
 const server = app.listen(3000, () => {
     console.log(`server is listening on PORT:`, server.address().port)
 })
